d1df26a5f219323ea8731b9fdc41e93e4a8c6fe4
harsha <harshavardhan.bhumi.com>
2021-12-09 11:48:13 +0530

    Added userprofile api

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -10,4 +10,15 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
         
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow user to update there own status"""
+
+    def has_object_permission(self, request,view, obj):
+        """Check the user is trying to update there uwn status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.user_profile.id == request.user.id
         
